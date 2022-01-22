342d6dd6c3ea27a94039d9859c5e6bf06bf47e2f
xxxhdd <98124345+xxxhdd@users.noreply.github.com>
2022-01-22 17:42:12 +0800

    pull commit

--- src/test/java/com/xhd/MybatisPlusApplicationTests.java
+++ src/test/java/com/xhd/MybatisPlusApplicationTests.java
@@ -144,6 +144,7 @@ class MybatisPlusApplicationTests {
         System.out.println("master test!");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 
 }
